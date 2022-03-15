178677cc450b0c2240a9d9c49f50bc09f44b216f
Federico Castellano <fcastellano@stash.com.ar>
2022-03-15 16:35:43 -0300

    COmmit inicial

---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { EstadisticaComponent } from '../ingreso-egreso/estadistica/estadistica.component';
+import { DetalleComponent } from '../ingreso-egreso/detalle/detalle.component';
+import { IngresoEgresoComponent } from '../ingreso-egreso/ingreso-egreso.component';
+
+
+export const dashboardroutes : Routes = [
+    {path : '', component : EstadisticaComponent},
+    {path : 'detalle', component : DetalleComponent},
+    {path: 'ingreso-egreso', component : IngresoEgresoComponent}
+
+
+]
